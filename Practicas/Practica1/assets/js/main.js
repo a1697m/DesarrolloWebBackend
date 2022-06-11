7ea72a871f660d77c7dc9bbfa37bb8a27e38f546
a1697m <mikeortizr@gmail.com>
2022-06-11 13:26:39 -0600

    Agregando practica 1

---
+++ Practicas/Practica1/assets/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var labelArtist = document.querySelector('.labelArtist');
+    var labelAlbum = document.querySelector('.labelAlbum');
+    var labelDesc = document.querySelector ('.labelDesc');
+    var labelImg = document.querySelector('.labelImg');
+    var songsContainer = document.querySelector('.songList');
+
+    async function getAlbumData(){
+        const request = await fetch('http://localhost:5500/Practicas/Practica1/assets/json/data.json');
+        var album = await request.json();
+        return album;
+    }
+
+    async function printAlbum(){
+        var album = await getAlbumData();
+        labelArtist.innerHTML = album.artista;
+        labelAlbum.innerHTML = album.album;
+        labelDesc.innerHTML = album.descripcion;
+        labelImg.setAttribute('src', album.imagen);
+        var row = '';
+        album.canciones.forEach(cancion => {
+            row +=`<tr> 
+                <td>${cancion.numero} </td>
+                <td>${cancion.titulo}</td>
+                <td>${cancion.duracion}</td>          
+            </tr>`;
+        });
+        songsContainer.innerHTML = row;
+    }
+    printAlbum();
+});
