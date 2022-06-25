108883154aa5467e6de34ae45b010a48c4a3457a
Efrain <26efrain26@gmail.com>
2022-06-25 12:39:47 -0600

    Termine :) si Funciono

--- Practicas/Practica2/assets/js/Secundario.js
+++ Practicas/Practica2/assets/js/Secundario.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var labeltipo = document.querySelector('.labeltipo');
     var labeldiasDePromocion = document.querySelector('.labeldiasDePromocion');
-    var labeldescripcion = document.querySelector('labeldescripcion');
+    var labeldescripcion = document.querySelector('.labeldescripcion');
     var labelImg = document.querySelector('.labelImg');
     var songsContainer = document.querySelector('.songList');
 
     async function getinData(){
-        const request = await fetch('http://127.0.0.1:5500/Practicas/Practica2/assets/json/CodigoPr.json')
+        const request = await fetch('http://127.0.0.1:5500/Practica2/assets/json/CodigoPr.json')
         var laptop = await request.json();
         return laptop;
     }
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         labeltipo.innerHTML = laptop.tipo;
         labeldiasDePromocion.innerHTML = laptop.diasDePromocion;
         labeldescripcion.innerHTML = laptop.descripcion;
-        labelImg.setAttribute('src', laptop.Imagen);
+        labelImg.setAttribute('src', laptop.imagen);
         var row = '';
-        laptop.Equipos.forEach(Equipo => {
+        laptop.equipos.forEach(Equipo => {
             row +=`<tr> 
                 <td>${Equipo.numero} </td>
                 <td>${Equipo.pantalla}</td>
